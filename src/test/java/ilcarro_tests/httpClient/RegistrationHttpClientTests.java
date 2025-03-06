47636ccfb61c8f336ba96c1a6e5f371dfe2f2a6b
Ivan <ivanalekseenko.post@gmail.com>
2025-03-06 09:01:11 +0100

    hw upd

--- src/test/java/ilcarro_tests/httpClient/RegistrationHttpClientTests.java
+++ src/test/java/ilcarro_tests/httpClient/RegistrationHttpClientTests.java
@@ -1,13 +1,10 @@
 package ilcarro_tests.httpClient;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
 import org.apache.http.entity.ContentType;
-import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,38 +12,50 @@ import java.io.IOException;
 
 public class RegistrationHttpClientTests {
     private static final String REGISTRATION_URL = "https://ilcarro-backend.herokuapp.com/v1/user/registration/usernamepassword";
-    private static final Gson GSON = new Gson();
 
     @Test
-    public void RegistrationSuccessTest() throws IOException {
+    public void RegistrationStatusCodeTest() throws IOException {
         String uniqueUsername = "user_" + System.currentTimeMillis() + "@qa.com";
-
         String jsonBody = """
                 {
                     "username": "%s",
                     "password": "Password@1",
-                    "firstName": "Test",
-                    "lastName": "User"
+                    "firstName": "Vasiliy",
+                    "lastName": "Vasin"
                 }
                 """.formatted(uniqueUsername);
 
-        // Получаем HttpResponse один раз
-        HttpResponse httpResponse = Request.Post(REGISTRATION_URL)
+        Response response = Request.Post(REGISTRATION_URL)
                 .bodyString(jsonBody, ContentType.APPLICATION_JSON)
-                .execute()
-                .returnResponse();
+                .execute();
 
-        // Проверка, что статус код равен 200
-        int statusCode = httpResponse.getStatusLine().getStatusCode();
-        Assert.assertEquals(statusCode, 200, "Статус код не равен 200");
+        int statusCode = response.returnResponse().getStatusLine().getStatusCode();
+        Assert.assertEquals(statusCode, 200, "Статус код отличается от 200");
+    }
 
-        // Считываем тело ответа в виде строки
-        String responseJson = EntityUtils.toString(httpResponse.getEntity());
+    @Test
+    public void RegistrationResponseFieldsTest() throws IOException {
+        String uniqueUsername = "user_" + System.currentTimeMillis() + "@qa.com";
+        String jsonBody = """
+                {
+                    "username": "%s",
+                    "password": "Password@1",
+                    "firstName": "Vasiliy",
+                    "lastName": "Vasin"
+                }
+                """.formatted(uniqueUsername);
+
+        Response response = Request.Post(REGISTRATION_URL)
+                .bodyString(jsonBody, ContentType.APPLICATION_JSON)
+                .execute();
+
+        String responseJson = response.returnContent().asString();
         JsonElement element = JsonParser.parseString(responseJson);
-        JsonElement accessToken = element.getAsJsonObject().get("accessToken");
 
-        // Проверка наличия токена и его значения
-        Assert.assertNotNull(accessToken, "Токен отсутствует в ответе");
-        Assert.assertFalse(accessToken.getAsString().isEmpty(), "Значение токена пустое");
+        Assert.assertTrue(element.getAsJsonObject().has("accessToken"), "Поле accessToken отсутствует в ответе");
+
+        String token = element.getAsJsonObject().get("accessToken").getAsString();
+        Assert.assertNotNull(token, "Значение accessToken null");
+        Assert.assertFalse(token.trim().isEmpty(), "Значение accessToken пустое");
     }
 }
